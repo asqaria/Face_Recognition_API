5f96ee63f7577ecc2fce434c6009f8636c08f525
asqaria <asqaria@hotmail.com>
2018-12-24 01:33:54 +0800

    return json after insertion

--- webserver.py
+++ webserver.py
@@ -16,7 +16,6 @@ session = scoped_session(DBSession)
 
 app = Flask(__name__)
 
-
 # Pre-trained face detection model here:
 predictor_model = "face_landmark.dat"
 
@@ -27,6 +26,7 @@ face_aligner = openface.AlignDlib(predictor_model)
 # Allowed extensions for images
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
+
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
@@ -34,39 +34,35 @@ def allowed_file(filename):
 
 @app.route('/create', methods=['POST'])
 def create():
-    if request.method == 'POST':
-        name = request.form['name']
-        email = request.form['email']
-        phone = request.form['phone']
-        picture = request.files.get('picture')
-        images = request.files.getlist('images')
+    name = request.form['name']
+    email = request.form['email']
+    phone = request.form['phone']
+    picture = request.files.get('picture')
+    images = request.files.getlist('images')
 
-        if picture and allowed_file(picture.filename):
-            extension = picture.filename.split('.')[1]
-            user = Users(name=name, email=email, phone=phone, image=extension)
-            session.add(user)
-            session.commit()
-            filename = '%s.%s' % (user.id, extension)
-            picture.save(os.path.join('images/users', filename))
-        else:
-            return 'Error while uploading picture'
-
-        for i in images:
-            # Run the HOG face detector for all images.
-            image = cv2.cvtColor(np.array(Image.open(i.stream)), cv2.COLOR_BGR2RGB)
-            detected_faces = face_detector(image, 1)
-
-            aligned_faces = list()
-
-            # Crop and align all faces
-            for i, face_rect in enumerate(detected_faces):
-                aligned_face = face_aligner.align(534, image, face_rect,
-                                                 landmarkIndices=openface.AlignDlib.OUTER_EYES_AND_NOSE)
-                aligned_faces.append(aligned_face)
-
-        return 'OK'
+    if picture and allowed_file(picture.filename):
+        extension = picture.filename.split('.')[1]
+        user = Users(name=name, email=email, phone=phone, image=extension)
+        session.add(user)
+        session.commit()
+        filename = '%s.%s' % (user.id, extension)
+        picture.save(os.path.join('images/users', filename))
     else:
-        return 'create'
+        return 'Error while uploading picture'
+
+    for i in images:
+        # Run the HOG face detector for all images.
+        image = cv2.cvtColor(np.array(Image.open(i.stream)), cv2.COLOR_BGR2RGB)
+        detected_faces = face_detector(image, 1)
+
+        aligned_faces = list()
+
+        # Crop and align all faces
+        for j, face_rect in enumerate(detected_faces):
+            aligned_face = face_aligner.align(534, image, face_rect, landmarkIndices=openface.AlignDlib.OUTER_EYES_AND_NOSE)
+            aligned_faces.append(aligned_face)
+
+    return jsonify(Users=[user.serialize])
 
 
 if __name__ == "__main__":
