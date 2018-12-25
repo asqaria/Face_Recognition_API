7bf0a8b3a9918fd6a77265c09a36091f8f8e2d0f
asqaria <asqaria@hotmail.com>
2018-12-25 14:35:26 +0800

    json return and some validtion added

--- webserver.py
+++ webserver.py
@@ -25,6 +25,8 @@ face_aligner = openface.AlignDlib(predictor_model)
 
 # Allowed extensions for images
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+# Minimum amount of images
+MIN_IMG = 2
 
 # model='nn4.small2.def.lua'
 net = openface.TorchNeuralNet(model='nn4.small2.v1.t7', imgDim=96, cuda=False)
@@ -43,32 +45,47 @@ def create():
     picture = request.files.get('picture')
     images = request.files.getlist('images')
 
-    if picture and allowed_file(picture.filename):
-        extension = picture.filename.split('.')[1]
-        user = Users(name=name, email=email, phone=phone, image=extension)
-        session.add(user)
-        session.commit()
-        filename = '%s.%s' % (user.id, extension)
-        picture.save(os.path.join('images/users', filename))
-    else:
-        return 'Error while uploading picture'
+    if name == '' or email == '' or phone == '' or len(images) < MIN_IMG or not picture:
+        return 'You are required to fill up all forms!'
+
+    total_images = len(images)
+    total_faces = 0
 
     for i in images:
         # Run the HOG face detector for all images.
         image = cv2.cvtColor(np.array(Image.open(i.stream)), cv2.COLOR_BGR2RGB)
         detected_faces = face_detector(image, 1)
+        total_faces += len(detected_faces)
 
-        aligned_faces = list()
-        i=1
+        matrix = list()
+
         # Crop and align all faces
         for j, face_rect in enumerate(detected_faces):
             aligned_face = face_aligner.align(96, image, face_rect, landmarkIndices=openface.AlignDlib.OUTER_EYES_AND_NOSE)
-            aligned_faces.append(aligned_face)
-            a = net.forward(aligned_face)
-            print(i)
-            i+=1
+            matrix.append(net.forward(aligned_face))
 
-    return jsonify(Users=[user.serialize])
+    if total_faces < MIN_IMG or total_images < MIN_IMG:
+        return 'Required minimum %d face images per user' % MIN_IMG
+
+    if picture and allowed_file(picture.filename):
+        extension = picture.filename.split('.')[1]
+        user = Users(name=name, email=email, phone=phone, image=extension)
+        session.add(user)
+        session.commit()
+        filename = 'picture.%s' % extension
+        path = 'images/users/%s' % user.id
+        os.mkdir(path)
+        picture.save(os.path.join(path, filename))
+    else:
+        return 'Error while uploading picture'
+
+    result = {
+        'id': user.id,
+        'images': total_images,
+        'detected_faces': total_faces
+    }
+
+    return jsonify(result)
 
 
 if __name__ == "__main__":
